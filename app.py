0fd56e10566d3997f3353eb2754c607ba434bfff
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2024-01-22 23:12:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import tensorflow_text as text
 tf.get_logger().setLevel('ERROR')
 def main():
     model = tf.saved_model.load('./models')
-    st.header('Hello da')
+    text = st.text_input('Comments')
 
 if __name__ == '__main__':
     main()
