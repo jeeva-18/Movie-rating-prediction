bb13ddbe31d72dbdf651c27a0c5769ffd9d9650c
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2024-01-22 23:18:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ tf.get_logger().setLevel('ERROR')
 def main():
     model = tf.saved_model.load('./models')
     text = st.text_input('Comments')
+    score = tf.sigmoid(reloaded_model(tf.constant(text)))
+    print(score)
 
 if __name__ == '__main__':
     main()
