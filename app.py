2530c9c178c77058437041ca620795102c1f1add
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2024-01-22 23:31:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ tf.get_logger().setLevel('ERROR')
 def main():
     model = tf.saved_model.load('./models')
     text = st.text_input('Comments')
-    if text == !None:
+    if text != None:
         score = tf.sigmoid(model(tf.constant(list(text))))
         st.write(score)
 
