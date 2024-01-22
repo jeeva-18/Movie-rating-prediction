828827724d3f7011520588d647836fd639a90e8d
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2024-01-22 23:33:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ import tensorflow_text as text
 tf.get_logger().setLevel('ERROR')
 def main():
     model = tf.saved_model.load('./models')
-    text = st.text_input('Comments')
-    if text != None:
-        score = tf.sigmoid(model(tf.constant(list(text))))
-        st.write(score)
+    text = ["happy happy happy "]
+    score = tf.sigmoid(model(tf.constant(text)))
+    st.write(score)
 
 if __name__ == '__main__':
     main()
